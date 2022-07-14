14d568f66edb0241c92be4ed212ef741e8a74fe2
siddharth raghav <thakurraghav27@gmail.com>
2022-07-14 16:01:37 +0530

    functions

--- starter/script.js
+++ starter/script.js
@@ -6,12 +6,45 @@ function op(p, n) {
 
 op("sa,sa");
 
-function nuni(p, n) {
-  const op = p + n;
-  return op;
-}
+// function declaration
 
-const consty = nuni("bod", "mashi");
+// function nuni(p, n) {
+//   const op = p + n;
+//   return op;
+// }
 
-console.log(consty);
-fdfd;
+// const consty = nuni("bod", "mashi");
+
+// console.log(consty);
+
+// function exp
+
+// const nuni = function (p, n) {
+//   const op = p + n;
+//   return op;
+// };
+
+// const consty = nuni("bod", "mashi");
+
+// console.log(consty);
+
+// arrow function
+
+// const nuni = (p) => console.log(`${p} is op`);
+
+// nuni("oooo");
+
+// functions calling others functions
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
