7e35787bc7433b6e0c6d7898ec06d247eaa579ec
siddharth raghav <thakurraghav27@gmail.com>
2022-07-14 15:34:38 +0530

    functions

---
+++ starter/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function op(p, n) {
+  console.log(p, n);
+}
+
+op("sa,sa");
+
+function nuni(p, n) {
+  const op = p + n;
+  return op;
+}
+
+const consty = nuni("bod", "mashi");
+
+console.log(consty);
+fdfd;
