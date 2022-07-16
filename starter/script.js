23de422e7131f366293f8874017e6b264d146562
siddharth raghav <thakurraghav27@gmail.com>
2022-07-16 13:08:56 +0530

    challange 4

--- starter/script.js
+++ starter/script.js
@@ -288,3 +288,28 @@ op("sa,sa");
 //   dice = Math.trunc(Math.random() * 6) + 1;
 //   if (dice === 6) console.log("Loop is about to end...");
 // }
+
+// coding challanges 4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+const calcavg = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcavg(totals));
+console.log(calcavg(tips));
